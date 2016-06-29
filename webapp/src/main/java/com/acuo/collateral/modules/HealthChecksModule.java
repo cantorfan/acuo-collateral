56ffcbdb93d8e7864992df40f43a382502d2db42
Hicham Medkouri <hicham@anaxo.io>
2016-06-29 10:31:54 +0100

    Added a Neo4j health check

--- webapp/src/main/java/com/acuo/collateral/modules/HealthChecksModule.java
+++ webapp/src/main/java/com/acuo/collateral/modules/HealthChecksModule.java
@@ -1,11 +1,19 @@
 package com.acuo.collateral.modules;
 
+import com.acuo.collateral.metrics.Neo4jHealthCheck;
 import com.acuo.common.metrics.DiskSpaceHealthCheck;
 import com.acuo.common.metrics.PingHealthCheck;
 import com.codahale.metrics.MetricSet;
 import com.codahale.metrics.health.HealthCheck;
 import com.google.inject.AbstractModule;
+import com.google.inject.Provides;
 import com.google.inject.multibindings.Multibinder;
+import org.glassfish.jersey.client.ClientConfig;
+import org.glassfish.jersey.client.ClientProperties;
+import org.glassfish.jersey.jackson.JacksonFeature;
+
+import javax.ws.rs.client.Client;
+import javax.ws.rs.client.ClientBuilder;
 
 public class HealthChecksModule extends AbstractModule {
     @Override
@@ -17,5 +25,17 @@ public class HealthChecksModule extends AbstractModule {
         final Multibinder<HealthCheck> healthChecksBinder = Multibinder.newSetBinder(binder(), HealthCheck.class);
         healthChecksBinder.addBinding().to(PingHealthCheck.class).asEagerSingleton();
         healthChecksBinder.addBinding().to(DiskSpaceHealthCheck.class).asEagerSingleton();
+        healthChecksBinder.addBinding().to(Neo4jHealthCheck.class).asEagerSingleton();
+    }
+
+    @Provides
+    Client getClient() {
+        ClientConfig config = new ClientConfig();
+        config.register(new JacksonFeature());
+        config.property(ClientProperties.ASYNC_THREADPOOL_SIZE, 10);
+        config.property(ClientProperties.CONNECT_TIMEOUT, 10000);
+        config.property(ClientProperties.READ_TIMEOUT, 10000);
+        Client client = ClientBuilder.newClient(config);
+        return client;
     }
 }
